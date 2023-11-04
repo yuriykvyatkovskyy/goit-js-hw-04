4d88554dbf699330434c3c68ab430e539f2e6a45
yuriykvyatkovskyy <yuriykreik20021997@gmail.com>
2023-11-04 17:57:23 +0200

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,16 @@
+function isEnoughCapacity(products, containerSize) {
+    let sum = 0;
+    for (const product of Object.values(products)) {
+        sum += product;    
+    }
+    if (sum > containerSize) {
+            return false;
+        } else {
+            return true;
+        }
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7));
